cff62f086012003101963e4bac3c820ab6fea3d7
Vladimir Davii <vladimir.davii@gmail.com>
2021-05-22 14:27:49 +0300

    start template create react app with typescript

---
+++ my-app/src/components/buttons/PrimaryButton/PrimaryButton.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const PrimaryButton = ({text}) => {
+    return (
+        <div>
+            {text}
+        </div>
+    );
+};
+
+PrimaryButton.propTypes = {
+
+};
+
+export { PrimaryButton };
